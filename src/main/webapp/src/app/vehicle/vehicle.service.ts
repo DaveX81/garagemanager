bf02ff2808586775c056a7b8b17a449db5fab2f4
David Pentzlin <david@pentzlin.net>
2021-05-07 11:38:19 +0200

    UI: Poll all vehicles

--- src/main/webapp/src/app/vehicle/vehicle.service.ts
+++ src/main/webapp/src/app/vehicle/vehicle.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Vehicle} from './Vehicle';
-import {Observable} from 'rxjs';
+import {Observable, timer} from 'rxjs';
 import {Configuration} from '../shared/configuration';
+import {switchMap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class VehicleService {
   constructor(private httpClient: HttpClient) { }
 
   public getAllVehicles(): Observable<Vehicle[]> {
-    return this.httpClient.get<Vehicle[]>(Configuration.REST_URL.concat('/vehicles'));
+    return timer(1, 5000).pipe(
+      switchMap(() => this.httpClient.get<Vehicle[]>(Configuration.REST_URL.concat('/vehicles')))
+    );
   }
 
   public addVehicle(vehicle: Vehicle): any {
